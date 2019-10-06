3942ab6315f383bae454108a1aafe8653f2bfb26
Khang Le <lekhang4497@gmail.com>
2019-10-06 11:28:43 +0700

    Updates

---
+++ precache-manifest.8a9b144a2cc2cdef6c5dac5a4f249621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a806052f9005ec47210054ede9ee618",
+    "url": "/ghem-site/index.html"
+  },
+  {
+    "revision": "03a67dc9ecec1931f1bd",
+    "url": "/ghem-site/static/css/main.8da36ca1.chunk.css"
+  },
+  {
+    "revision": "3c124e112944162db42f",
+    "url": "/ghem-site/static/js/2.f11d9db7.chunk.js"
+  },
+  {
+    "revision": "03a67dc9ecec1931f1bd",
+    "url": "/ghem-site/static/js/main.ddbdfc4c.chunk.js"
+  },
+  {
+    "revision": "61fd19105bac853bf8b6",
+    "url": "/ghem-site/static/js/runtime-main.eb452ecf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ghem-site/static/media/logo.25bf045c.svg"
+  }
+]);
