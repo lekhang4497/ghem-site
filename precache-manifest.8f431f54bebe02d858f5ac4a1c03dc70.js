e546aaaa1fec9c493f00a307b758e2d12530dab0
Khang Le <lekhang4497@gmail.com>
2019-10-12 18:17:22 +0700

    Updates

---
+++ precache-manifest.8f431f54bebe02d858f5ac4a1c03dc70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d04eae402262880efc1ed9e4c1ee47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d8a605a02723f8cac68",
+    "url": "/static/css/main.8017684b.chunk.css"
+  },
+  {
+    "revision": "5f07ee585b088b4bb049",
+    "url": "/static/js/2.8ab79a4c.chunk.js"
+  },
+  {
+    "revision": "8d8a605a02723f8cac68",
+    "url": "/static/js/main.7b72d908.chunk.js"
+  },
+  {
+    "revision": "d354a13334a3bb7be8de",
+    "url": "/static/js/runtime-main.38af263d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
